b150903b315069118c110eabecaf22bac39a7239
luca <lucacambi77@gmail.com>
2019-10-07 10:24:26 +0200

    equals and hashcode lombok

--- src/test/java/it/cambi/codility/coreJava/DataKeyEquals.java
+++ src/test/java/it/cambi/codility/coreJava/DataKeyEquals.java
@@ -3,8 +3,8 @@
  */
 package it.cambi.codility.coreJava;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
@@ -14,44 +14,17 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-public class DataKeyEquals {
+@EqualsAndHashCode
+public class DataKeyEquals
+{
 
-	protected String name;
-	protected int id;
+    protected String name;
+    protected int id;
 
-	// getter and setter methods
+    @Override
+    public String toString()
+    {
+        return "DataKey [name=" + name + ", id=" + id + "]";
+    }
 
-	@Override
-	public String toString() {
-		return "DataKey [name=" + name + ", id=" + id + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		DataKeyEquals other = (DataKeyEquals) obj;
-		if (id != other.id)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
-	}
 }
