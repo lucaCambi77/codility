ac164a41951f685c8e4a15da2fc60359fd7c1561
luca <lucacambi77@gmail.com>
2019-10-11 17:13:57 +0200

    new formatting applied

--- src/test/java/it/cambi/codility/model/BinaryTree.java
+++ src/test/java/it/cambi/codility/model/BinaryTree.java
@@ -7,139 +7,159 @@ package it.cambi.codility.model;
  * @author luca
  *
  */
-public class BinaryTree {
+public class BinaryTree
+{
 
-	private Node root;
+    private Node root;
 
-	public BinaryTree(int key) {
-		root = new Node(key);
-	};
+    public BinaryTree(int key)
+    {
+        root = new Node(key);
+    };
 
-	/*
-	 * Given a binary tree. Print its nodes in level order using array for
-	 * implementing queue
-	 */
-	int size() {
-		return size(root);
-	}
+    /*
+     * Given a binary tree. Print its nodes in level order using array for implementing queue
+     */
+    int size()
+    {
+        return size(root);
+    }
 
-	/* computes number of nodes in tree */
-	int size(Node node) {
-		if (node == null)
-			return 0;
-		else
-			return (size(node.left) + 1 + size(node.right));
-	}
+    /* computes number of nodes in tree */
+    int size(Node node)
+    {
+        if (node == null)
+            return 0;
+        else
+            return (size(node.left) + 1 + size(node.right));
+    }
 
-	// This method mainly calls insertRec()
-	void insert(int key) {
-		root = insertRec(root, key);
-	}
+    // This method mainly calls insertRec()
+    void insert(int key)
+    {
+        root = insertRec(root, key);
+    }
 
-	/* A recursive function to insert a new key in BST */
-	Node insertRec(Node root, int key) {
+    /* A recursive function to insert a new key in BST */
+    Node insertRec(Node root, int key)
+    {
 
-		/* If the tree is empty, return a new node */
-		if (root == null) {
-			root = new Node(key);
-			return root;
-		}
+        /* If the tree is empty, return a new node */
+        if (root == null)
+        {
+            root = new Node(key);
+            return root;
+        }
 
-		/* Otherwise, recur down the tree */
-		if (key < root.data)
-			root.setLeft(insertRec(root.left, key));
-		else if (key > root.data)
-			root.setRight(insertRec(root.right, key));
+        /* Otherwise, recur down the tree */
+        if (key < root.data)
+            root.setLeft(insertRec(root.left, key));
+        else if (key > root.data)
+            root.setRight(insertRec(root.right, key));
 
-		/* return the (unchanged) node pointer */
-		return root;
-	}
+        /* return the (unchanged) node pointer */
+        return root;
+    }
 
-	// A utility function to search a given key in BST
-	public Node search(Node root, int key) {
-		// Base Cases: root is null or key is present at root
-		if (root == null || root.data == key)
-			return root;
+    // A utility function to search a given key in BST
+    public Node search(Node root, int key)
+    {
+        // Base Cases: root is null or key is present at root
+        if (root == null || root.data == key)
+            return root;
 
-		// val is greater than root's key
-		if (root.data > key)
-			return search(root.left, key);
+        // val is greater than root's key
+        if (root.data > key)
+            return search(root.left, key);
 
-		// val is less than root's key
-		return search(root.right, key);
-	}
+        // val is less than root's key
+        return search(root.right, key);
+    }
 
-	/**
-	 * 
-	 */
-	public void inOrderTrasv() {
+    /**
+     * 
+     */
+    public void inOrderTrasv()
+    {
 
-		if (root != null) {
-			inOrderTrasv(root.left);
-			System.out.println(root.data);
-			inOrderTrasv(root.right);
-		}
-	}
+        if (root != null)
+        {
+            inOrderTrasv(root.left);
+            System.out.println(root.data);
+            inOrderTrasv(root.right);
+        }
+    }
 
-	private void inOrderTrasv(Node root) {
+    private void inOrderTrasv(Node root)
+    {
 
-		if (root != null) {
-			inOrderTrasv(root.left);
-			System.out.println(root.data);
-			inOrderTrasv(root.right);
-		}
-	}
+        if (root != null)
+        {
+            inOrderTrasv(root.left);
+            System.out.println(root.data);
+            inOrderTrasv(root.right);
+        }
+    }
 
-	public void preOrderTrasv() {
+    public void preOrderTrasv()
+    {
 
-		if (root != null) {
-			System.out.println(root.data);
-			preOrderTrasv(root.left);
-			preOrderTrasv(root.right);
-		}
-	}
+        if (root != null)
+        {
+            System.out.println(root.data);
+            preOrderTrasv(root.left);
+            preOrderTrasv(root.right);
+        }
+    }
 
-	private void preOrderTrasv(Node root) {
+    private void preOrderTrasv(Node root)
+    {
 
-		if (root != null) {
-			System.out.println(root.data);
-			preOrderTrasv(root.left);
-			preOrderTrasv(root.right);
-		}
-	}
+        if (root != null)
+        {
+            System.out.println(root.data);
+            preOrderTrasv(root.left);
+            preOrderTrasv(root.right);
+        }
+    }
 
-	public void postOrderTrasv() {
+    public void postOrderTrasv()
+    {
 
-		if (root != null) {
-			postOrderTrasv(root.left);
-			postOrderTrasv(root.right);
-			System.out.println(root.data);
-		}
-	}
+        if (root != null)
+        {
+            postOrderTrasv(root.left);
+            postOrderTrasv(root.right);
+            System.out.println(root.data);
+        }
+    }
 
-	private void postOrderTrasv(Node root) {
+    private void postOrderTrasv(Node root)
+    {
 
-		if (root != null) {
-			preOrderTrasv(root.left);
-			preOrderTrasv(root.right);
-			System.out.println(root.data);
-		}
-	}
+        if (root != null)
+        {
+            preOrderTrasv(root.left);
+            preOrderTrasv(root.right);
+            System.out.println(root.data);
+        }
+    }
 
-	public static Node getBST() {
-		Node root = new Node(50);
+    public static Node getBST()
+    {
+        Node root = new Node(50);
 
-		Node left = new Node(30);
-		left.setLeft(new Node(20));
-		left.setRight(new Node(40));
+        Node left = new Node(30);
+        left.setRight(new Node(40));
+        left.setLeft(new Node(20));
 
-		root.setLeft(left);
+        root.setLeft(left);
 
-		Node right = new Node(70);
-		right.setRight(new Node(80));
-		right.setLeft(new Node(60));
+        Node right = new Node(70);
+        right.setRight(new Node(80));
+        right.setLeft(new Node(60));
 
-		root.setRight(right);
-		return root;
-	}
+        root.setRight(right);
+        return root;
+    }
 }
