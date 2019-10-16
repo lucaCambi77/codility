6ebfd58bee09730b017628b223cf04f8cb750896
luca <lucacambi77@gmail.com>
2019-10-16 22:35:49 +0200

    common palindrome method

--- src/test/java/it/cambi/codility/company/Ocado.java
+++ src/test/java/it/cambi/codility/company/Ocado.java
@@ -1,204 +1,208 @@
 package it.cambi.codility.company;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
-public class Ocado {
+public class Ocado
+{
 
-	private static String palindromeString = "aabaa";
-	private static String notPalindromeString = "cdefghmnopqrstuvw";
-	private static String canArrangePalindromeString = "aaabbbb";
+    private static String notPalindromeString = "cdefghmnopqrstuvw";
+    private static String canArrangePalindromeString = "aaabbbb";
 
-	@Test
-	public void isPalindrome() {
-		boolean isPalindrome = true;
+    @Test
+    public void canArrangeAsPalindrome()
+    {
 
-		int lengthOf = palindromeString.length();
-		int middlePoint = ((lengthOf & 1) == 1) ? lengthOf / 2 : (lengthOf + 1) / 2;
+        assertEquals(true, canArrangePalindrome(canArrangePalindromeString));
+        assertEquals(false, canArrangePalindrome(notPalindromeString));
 
-		for (int i = lengthOf - 1; i >= middlePoint; i--) {
-			if (palindromeString.charAt(i) != palindromeString.charAt(lengthOf - i - 1)) {
-				isPalindrome = false;
-				break;
+    }
 
-			}
+    /**
+     * @return
+     */
+    private boolean canArrangePalindrome(String canArrangePalindromeString)
+    {
+        boolean canArrange = true;
 
-		}
+        int count[] = new int[128];
+        Arrays.fill(count, 0);
 
-		assertTrue(isPalindrome);
-	}
+        // For each character in input strings,
+        // increment count in the corresponding
+        // count array
+        for (int i = 0; i < canArrangePalindromeString.length(); i++)
+            count[(int) (canArrangePalindromeString.charAt(i))]++;
 
-	@Test
-	public void canArrangeAsPalindrome() {
+        int alphabetCountLength = count.length;
 
-		assertEquals(true, canArrangePalindrome(canArrangePalindromeString));
-		assertEquals(false, canArrangePalindrome(notPalindromeString));
+        // Count odd occurring characters
+        int odd = 0;
+        for (int i = 0; i < alphabetCountLength; i++)
+        {
+            if ((count[i] & 1) == 1)
+                odd++;
 
-	}
+            if (odd > 1)
+                canArrange = false;
+        }
+        return canArrange;
+    }
 
-	/**
-	 * @return
-	 */
-	private boolean canArrangePalindrome(String canArrangePalindromeString) {
-		boolean canArrange = true;
+    @Test
+    public void longestPalindrome()
+    {
 
-		int count[] = new int[128];
-		Arrays.fill(count, 0);
+        longestPalSubstr("baabaaa");
+    }
 
-		// For each character in input strings,
-		// increment count in the corresponding
-		// count array
-		for (int i = 0; i < canArrangePalindromeString.length(); i++)
-			count[(int) (canArrangePalindromeString.charAt(i))]++;
+    // This function prints the longest palindrome substring
+    // of str[].
+    // It also returns the length of the longest palindrome
+    static int longestPalSubstr(String str)
+    {
+        int n = str.length(); // get length of input string
 
-		int alphabetCountLength = count.length;
+        // table[i][j] will be false if substring str[i..j]
+        // is not palindrome.
+        // Else table[i][j] will be true
+        boolean table[][] = new boolean[n][n];
 
-		// Count odd occurring characters
-		int odd = 0;
-		for (int i = 0; i < alphabetCountLength; i++) {
-			if ((count[i] & 1) == 1)
-				odd++;
+        // All substrings of length 1 are palindromes
+        int maxLength = 1;
+        for (int i = 0; i < n; ++i)
+            table[i][i] = true;
 
-			if (odd > 1)
-				canArrange = false;
-		}
-		return canArrange;
-	}
+        // check for sub-string of length 2.
+        int start = 0;
+        for (int i = 0; i < n - 1; ++i)
+        {
+            if (str.charAt(i) == str.charAt(i + 1))
+            {
+                table[i][i + 1] = true;
+                start = i;
+                maxLength = 2;
+            }
+        }
 
-	@Test
-	public void longestPalindrome() {
+        // Check for lengths greater than 2. k is length
+        // of substring
+        for (int k = 3; k <= n; ++k)
+        {
 
-		longestPalSubstr("baabaaa");
-	}
+            // Fix the starting index
+            for (int i = 0; i < n - k + 1; ++i)
+            {
+                // Get the ending index of substring from
+                // starting index i and length k
+                int j = i + k - 1;
 
-	// This function prints the longest palindrome substring
-	// of str[].
-	// It also returns the length of the longest palindrome
-	static int longestPalSubstr(String str) {
-		int n = str.length(); // get length of input string
+                // checking for sub-string from ith index to
+                // jth index iff str.charAt(i+1) to
+                // str.charAt(j-1) is a palindrome
+                if (table[i + 1][j - 1] && str.charAt(i) == str.charAt(j))
+                {
+                    table[i][j] = true;
 
-		// table[i][j] will be false if substring str[i..j]
-		// is not palindrome.
-		// Else table[i][j] will be true
-		boolean table[][] = new boolean[n][n];
+                    if (k > maxLength)
+                    {
+                        start = i;
+                        maxLength = k;
+                    }
+                }
+            }
+        }
+        System.out.print("Longest palindrome substring is; ");
+        System.out.println(str.substring(start, start + maxLength));
 
-		// All substrings of length 1 are palindromes
-		int maxLength = 1;
-		for (int i = 0; i < n; ++i)
-			table[i][i] = true;
+        return maxLength; // return length of LPS
+    }
 
-		// check for sub-string of length 2.
-		int start = 0;
-		for (int i = 0; i < n - 1; ++i) {
-			if (str.charAt(i) == str.charAt(i + 1)) {
-				table[i][i + 1] = true;
-				start = i;
-				maxLength = 2;
-			}
-		}
+    public void longestPalindrome1()
+    {
+        String str = "baabaaa";
+        // Java implementation of O(n^2) time and O(1) space method
+        // to find the longest palindromic substring
+        int maxLength = 1; // The result (length of LPS)
 
-		// Check for lengths greater than 2. k is length
-		// of substring
-		for (int k = 3; k <= n; ++k) {
+        int start = 0;
+        int len = str.length();
 
-			// Fix the starting index
-			for (int i = 0; i < n - k + 1; ++i) {
-				// Get the ending index of substring from
-				// starting index i and length k
-				int j = i + k - 1;
+        int low, high;
 
-				// checking for sub-string from ith index to
-				// jth index iff str.charAt(i+1) to
-				// str.charAt(j-1) is a palindrome
-				if (table[i + 1][j - 1] && str.charAt(i) == str.charAt(j)) {
-					table[i][j] = true;
+        // One by one consider every character as center
+        // point of even and length palindromes
+        for (int i = 1; i < len; ++i)
+        {
+            // Find the longest even length palindrome with
+            // center points as i-1 and i.
+            low = i - 1;
+            high = i;
+            while (low >= 0 && high < len && str.charAt(low) == str.charAt(high))
+            {
+                if (high - low + 1 > maxLength)
+                {
+                    start = low;
+                    maxLength = high - low + 1;
+                }
+                --low;
+                ++high;
+            }
 
-					if (k > maxLength) {
-						start = i;
-						maxLength = k;
-					}
-				}
-			}
-		}
-		System.out.print("Longest palindrome substring is; ");
-		System.out.println(str.substring(start, start + maxLength));
+            // Find the longest odd length palindrome with
+            // center point as i
+            low = i - 1;
+            high = i + 1;
+            while (low >= 0 && high < len && str.charAt(low) == str.charAt(high))
+            {
+                if (high - low + 1 > maxLength)
+                {
+                    start = low;
+                    maxLength = high - low + 1;
+                }
+                --low;
+                ++high;
+            }
+        }
 
-		return maxLength; // return length of LPS
-	}
+        System.out.print("Longest palindrome substring is: " + str.substring(start, start + maxLength - 1));
 
-	public void longestPalindrome1() {
-		String str = "baabaaa";
-		// Java implementation of O(n^2) time and O(1) space method
-		// to find the longest palindromic substring
-		int maxLength = 1; // The result (length of LPS)
+        // This code is contributed by Sumit Ghosh
 
-		int start = 0;
-		int len = str.length();
+    }
 
-		int low, high;
+    @Test
+    public void primitiveBetweenNumbers()
+    {
+        // Declare the variables
+        int s1, s2, i, j, count = 0;
 
-		// One by one consider every character as center
-		// point of even and length palindromes
-		for (int i = 1; i < len; ++i) {
-			// Find the longest even length palindrome with
-			// center points as i-1 and i.
-			low = i - 1;
-			high = i;
-			while (low >= 0 && high < len && str.charAt(low) == str.charAt(high)) {
-				if (high - low + 1 > maxLength) {
-					start = low;
-					maxLength = high - low + 1;
-				}
-				--low;
-				++high;
-			}
+        s1 = 5;
+        s2 = 20;
 
-			// Find the longest odd length palindrome with
-			// center point as i
-			low = i - 1;
-			high = i + 1;
-			while (low >= 0 && high < len && str.charAt(low) == str.charAt(high)) {
-				if (high - low + 1 > maxLength) {
-					start = low;
-					maxLength = high - low + 1;
-				}
-				--low;
-				++high;
-			}
-		}
+        for (i = s1; i <= s2; i++)
+        {
+            for (j = 2; j < i; j++)
+            {
+                if (i % j == 0)
+                {
+                    count = 0;
+                    break;
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+            if (count == 1)
+            {
+                System.out.println(i);
+            }
+        }
 
-		System.out.print("Longest palindrome substring is: " + str.substring(start, start + maxLength - 1));
-
-		// This code is contributed by Sumit Ghosh
-
-	}
-
-	@Test
-	public void primitiveBetweenNumbers() {
-		// Declare the variables
-		int s1, s2, i, j, count = 0;
-
-		s1 = 5;
-		s2 = 20;
-
-		for (i = s1; i <= s2; i++) {
-			for (j = 2; j < i; j++) {
-				if (i % j == 0) {
-					count = 0;
-					break;
-				} else {
-					count = 1;
-				}
-			}
-			if (count == 1) {
-				System.out.println(i);
-			}
-		}
-
-	}
+    }
 
 }
---
+++ src/test/java/it/cambi/codility/coreJava/StringTest.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package it.cambi.codility.coreJava;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author luca
+ *
+ */
+public class StringTest
+{
+
+    @Test
+    public void isPalindrome()
+    {
+        assertTrue(isPalindrome("aaaa"));
+        assertFalse(isPalindrome("aaaab"));
+        assertTrue(isPalindrome("aa"));
+        assertTrue(isPalindrome("a"));
+
+    }
+
+    /**
+     * @return
+     */
+    public static boolean isPalindrome(String palindromeString)
+    {
+        boolean isPalindrome = true;
+
+        int lengthOf = palindromeString.length();
+        int middlePoint = ((lengthOf & 1) == 1) ? lengthOf / 2 : (lengthOf + 1) / 2;
+
+        for (int i = lengthOf - 1; i >= middlePoint; i--)
+        {
+            if (palindromeString.charAt(i) != palindromeString.charAt(lengthOf - i - 1))
+            {
+                isPalindrome = false;
+                break;
+
+            }
+
+        }
+        return isPalindrome;
+    }
+}
