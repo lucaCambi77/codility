41c033f136bc0339318c625884c0d183346b52c2
luca <lucacambi77@gmail.com>
2019-11-09 23:51:18 +0100

    addBinary

--- src/test/java/it/cambi/codility/leetcode/LeetCodeStringTest.java
+++ src/test/java/it/cambi/codility/leetcode/LeetCodeStringTest.java
@@ -27,6 +27,48 @@ public class LeetCodeStringTest
 {
 
     @Test
+    public void addBinary()
+    {
+        assertEquals("100", addBinary("11", "1"));
+        assertEquals("10101", addBinary("1010", "1011"));
+
+    }
+
+    private String addBinary(String a, String b)
+    {
+        // Initialize result
+        String result = "";
+
+        // Initialize digit sum
+        int s = 0;
+
+        // Traverse both strings starting
+        // from last characters
+        int i = a.length() - 1, j = b.length() - 1;
+        while (i >= 0 || j >= 0 || s == 1)
+        {
+
+            // Comput sum of last
+            // digits and carry
+            s += ((i >= 0) ? a.charAt(i) - '0' : 0);
+            s += ((j >= 0) ? b.charAt(j) - '0' : 0);
+
+            // If current digit sum is
+            // 1 or 3, add 1 to result
+            result = (char) (s % 2 + '0') + result;
+
+            // Compute carry
+            s /= 2;
+
+            // Move to next digits
+            i--;
+            j--;
+        }
+
+        return result;
+    }
+
+    @Test
     public void reverseString()
     {
         char[] array = new char[] { 'h', 'e', 'l', 'l', 'o' };
