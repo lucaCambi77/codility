5d15437aa34d5629040aba9f5bd890ace7ebfa1d
luca <lucacambi77@gmail.com>
2019-11-10 22:21:55 +0100

    rangeSumBST

--- src/test/java/it/cambi/codility/leetcode/LeetCodeTreeTest.java
+++ src/test/java/it/cambi/codility/leetcode/LeetCodeTreeTest.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.jupiter.api.Test;
 
@@ -21,6 +22,42 @@ import it.cambi.codility.model.Node;
  */
 public class LeetCodeTreeTest
 {
+
+    @Test
+    public void rangeSumBST()
+    {
+        Node root = new Node(10);
+
+        Node left = new Node(5);
+        left.setLeft(new Node(3));
+        left.setRight(new Node(7));
+
+        root.setLeft(left);
+
+        Node right = new Node(15);
+        right.setRight(new Node(18));
+
+        root.setRight(right);
+
+        AtomicInteger sum = new AtomicInteger(0);
+        rangeSumBST(root, 7, 15, sum);
+
+        assertEquals(32, sum.get());
+    }
+
+    public void rangeSumBST(Node root, int left, int right, AtomicInteger sum)
+    {
+
+        if (root == null)
+            return;
+
+        if (root.data >= left && root.data <= right)
+            sum.addAndGet(root.data);
+
+        rangeSumBST(root.left, left, right, sum);
+        rangeSumBST(root.right, left, right, sum);
+    }
+
     @Test
     public void findTarget()
     {
