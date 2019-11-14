1d770bef661619a5be52cc0760012f34be5c24bd
luca <lucacambi77@gmail.com>
2019-11-14 15:13:25 +0100

    factorial for big numbers

--- src/test/java/it/cambi/codility/coreJava/Recursion.java
+++ src/test/java/it/cambi/codility/coreJava/Recursion.java
@@ -5,49 +5,57 @@ package it.cambi.codility.coreJava;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.math.BigInteger;
+
 import org.junit.jupiter.api.Test;
 
 /**
  * @author luca
  *
  */
-public class Recursion {
+public class Recursion
+{
 
-	@Test
-	public void factorial() {
+    @Test
+    public void factorial()
+    {
 
-		int n = 5;
+        assertEquals(120, factorial(new BigInteger("5")).intValue());
+        assertEquals("6227020800", factorial(new BigInteger("13")).toString());
+        assertEquals("265252859812191058636308480000000", factorial(new BigInteger("30")).toString());
+        assertEquals("3628800", factorial(new BigInteger("10")).toString());
 
-		assertEquals(120, factorial(n));
-	}
+    }
 
-	public int factorial(int n) {
+    public BigInteger factorial(BigInteger n)
+    {
 
-		if (n < 0)
-			return 0;
+        if (n.compareTo(new BigInteger("1")) == 0)
+            return new BigInteger("1");
 
-		if (n == 0)
-			return 1;
+        String number = factorial(n.subtract(new BigInteger("1"))).toString();
 
-		return n * factorial(n - 1);
-	}
+        return n.multiply(new BigInteger(number));
+    }
 
-	@Test
-	public void power() {
+    @Test
+    public void power()
+    {
 
-		double n = 2;
-		int p = 5;
+        double n = 2;
+        int p = 5;
 
-		assertEquals(32.0, power(n, p));
+        assertEquals(32.0, power(n, p));
 
-	}
+    }
 
-	public double power(double n, int p) {
+    public double power(double n, int p)
+    {
 
-		if (p == 0)
-			return 1.0;
+        if (p == 0)
+            return 1.0;
 
-		return n * power(n, p - 1);
-	}
+        return n * power(n, p - 1);
+    }
 
 }
