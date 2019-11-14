864d7f712b2f994300d8c527db84f061b536af1d
luca <lucacambi77@gmail.com>
2019-11-14 09:37:36 +0100

    zero even odd

--- src/test/java/it/cambi/codility/leetcode/LeetCodeMultithreading.java
+++ src/test/java/it/cambi/codility/leetcode/LeetCodeMultithreading.java
@@ -9,6 +9,7 @@ import static org.mockito.Mockito.times;
 import java.io.PrintStream;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Semaphore;
+import java.util.function.IntConsumer;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Order;
@@ -252,6 +253,224 @@ public class LeetCodeMultithreading
             // printBar.run() outputs "bar". Do not change or remove this line.
             printBar.run();
             runFoo.release();
+        }
+
+    }
+
+    @Test
+    @Order(3)
+    public void zeroEvenOdd() throws InterruptedException
+    {
+        out.flush();
+
+        ZeroEvenOdd instance = new ZeroEvenOdd(2);
+
+        ThreadA threada = new ThreadA(instance);
+        ThreadB threadb = new ThreadB(instance);
+        ThreadC threadc = new ThreadC(instance);
+
+        threada.start();
+        threadb.start();
+        threadc.start();
+
+        Thread.sleep(2000);
+
+        InOrder orderVerifier = Mockito.inOrder(out);
+        orderVerifier.verify(out).print(0);
+        orderVerifier.verify(out).print(1);
+        orderVerifier.verify(out).print(0);
+        orderVerifier.verify(out).print(2);
+    }
+
+    class ZeroEvenOdd
+    {
+
+        // have one permit
+        private Semaphore zeroSema = new Semaphore(1);
+        // have no permit at start
+        private Semaphore evenSema = new Semaphore(0);
+        // have no permit at start
+        private Semaphore oddSema = new Semaphore(2);
+
+        private int n;
+
+        public ZeroEvenOdd(int n)
+        {
+            this.n = n;
+        }
+
+        // printNumber.accept(x) outputs "x", where x is an integer.
+        public void zero(ZeroEvenOddConsumer printNumber) throws InterruptedException
+        {
+            for (int i = 0; i < n; i++)
+            {
+                zeroSema.acquire(1);
+
+                printNumber.accept(0);
+
+                evenSema.release();
+                oddSema.release();
+            }
+        }
+
+        public void even(ZeroEvenOddConsumer printNumber) throws InterruptedException
+        {
+            for (int i = 2; i <= n; i += 2)
+            {
+                evenSema.acquire(2);
+
+                printNumber.accept(i);
+
+                zeroSema.release();
+                oddSema.release();
+            }
+        }
+
+        public void odd(ZeroEvenOddConsumer printNumber) throws InterruptedException
+        {
+            for (int i = 1; i <= n; i += 2)
+            {
+                oddSema.acquire(3);
+
+                printNumber.accept(i);
+
+                zeroSema.release();
+            }
+        }
+
+    }
+
+    class ThreadA extends Thread
+    {
+
+        private ZeroEvenOdd zeroEvenOdd;
+        private PrintZeroConsumer consumer = new PrintZeroConsumer();
+
+        public ThreadA(ZeroEvenOdd zeroEvenOdd)
+        {
+            this.zeroEvenOdd = zeroEvenOdd;
+
+        }
+
+        @Override
+        public void run()
+        {
+            try
+            {
+                zeroEvenOdd.zero(consumer);
+            }
+            catch (InterruptedException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class ThreadB extends Thread
+    {
+
+        private ZeroEvenOdd zeroEvenOdd;
+        private PrintEvenConsumer consumer = new PrintEvenConsumer();
+
+        public ThreadB(ZeroEvenOdd zeroEvenOdd)
+        {
+            this.zeroEvenOdd = zeroEvenOdd;
+
+        }
+
+        @Override
+        public void run()
+        {
+            try
+            {
+                zeroEvenOdd.even(consumer);
+            }
+            catch (InterruptedException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    class ThreadC extends Thread
+    {
+
+        private ZeroEvenOdd zeroEvenOdd;
+        private PrintOddConsumer consumer = new PrintOddConsumer();
+
+        public ThreadC(ZeroEvenOdd zeroEvenOdd)
+        {
+            this.zeroEvenOdd = zeroEvenOdd;
+
+        }
+
+        @Override
+        public void run()
+        {
+            try
+            {
+                zeroEvenOdd.odd(consumer);
+            }
+            catch (InterruptedException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    interface ZeroEvenOddConsumer extends IntConsumer
+    {
+
+        void accept(int s);
+
+    }
+
+    class PrintZeroConsumer implements ZeroEvenOddConsumer
+    {
+
+        @Override
+        public void accept(int s)
+        {
+            if (s == 0)
+                System.out.print(s);
+            else
+                throw new RuntimeException();
+
+        }
+
+    }
+
+    class PrintOddConsumer implements ZeroEvenOddConsumer
+    {
+
+        @Override
+        public void accept(int s)
+        {
+            if ((s & 1) == 1)
+                System.out.print(s);
+            else
+                throw new RuntimeException();
+
+        }
+
+    }
+
+    class PrintEvenConsumer implements ZeroEvenOddConsumer
+    {
+
+        @Override
+        public void accept(int s)
+        {
+            if ((s & 1) == 0)
+                System.out.print(s);
+            else
+                throw new RuntimeException();
+
         }
 
     }
