9ae3b58560b55bff177a7fa09142e9dc26408417
luca <lucacambi77@gmail.com>
2019-11-20 12:03:28 +0100

    basic unit test for trie

--- src/test/java/it/cambi/codility/model/TrieTest.java
+++ src/test/java/it/cambi/codility/model/TrieTest.java
@@ -3,25 +3,29 @@
  */
 package it.cambi.codility.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 /**
  * @author luca
  *
  */
-public class TrieTest {
+public class TrieTest
+{
 
-	@Test
-	public void treeTest() {
-		Trie trie = new Trie();
-		String keys[] = { "the", "a", "there", "answer", "any", "by", "bye", "their" };
+    @Test
+    public void treeTest()
+    {
+        Trie trie = new Trie();
+        String keys[] = { "the", "a", "there", "answer", "any", "by", "bye", "their" };
 
-		String output[] = { "Not present in trie", "Present in trie" };
+        int i;
+        for (i = 0; i < keys.length; i++)
+            trie.insert(keys[i]);
 
-		int i;
-		for (i = 0; i < keys.length; i++)
-			trie.insert(keys[i]);
+        assertEquals(true, trie.search("their"));
+        assertEquals(false, trie.search("theirs"));
 
-		System.out.println(trie);
-	}
+    }
 }
