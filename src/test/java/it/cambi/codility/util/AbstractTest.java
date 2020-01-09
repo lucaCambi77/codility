1962af7bb442f8b64a73996d623c043b3e078713
l.cambi <l.cambi>
2020-01-09 14:37:48 +0100

    carriage return by os type added

--- src/test/java/it/cambi/codility/util/AbstractTest.java
+++ src/test/java/it/cambi/codility/util/AbstractTest.java
@@ -4,45 +4,50 @@ public class AbstractTest {
 
 	private static String unixCarriage = "\n";
 	private static String windowsCarriage = "\r\n";
+	private static String carriageReturn;
 
 	public enum OSType {
 		Windows, MacOS, Linux, Other
 	};
 
-	// cached result of OS detection
-	protected static OSType detectedOS;
-
-	public static String getCarriageReturn() {
+	static {
 
 		switch (getOperatingSystemType()) {
 		case Windows:
 
-			return windowsCarriage;
+			carriageReturn = windowsCarriage;
+			break;
 
 		case Linux:
 
-			return unixCarriage;
+			carriageReturn = unixCarriage;
+			break;
 
 		default:
 			break;
 		}
 
-		return null;
+	}
+
+	protected static OSType detectedOS;
+
+	public static String getCarriageReturn() {
+		return carriageReturn;
 	}
 
 	public static OSType getOperatingSystemType() {
-		if (detectedOS == null) {
-			String OS = System.getProperty("os.name", "generic").toLowerCase();
-			if ((OS.indexOf("mac") >= 0) || (OS.indexOf("darwin") >= 0)) {
-				detectedOS = OSType.MacOS;
-			} else if (OS.indexOf("win") >= 0) {
-				detectedOS = OSType.Windows;
-			} else if (OS.indexOf("nux") >= 0) {
-				detectedOS = OSType.Linux;
-			} else {
-				detectedOS = OSType.Other;
-			}
+		
+		String OS = System.getProperty("os.name", "generic").toLowerCase();
+		if ((OS.indexOf("mac") >= 0) || (OS.indexOf("darwin") >= 0)) {
+			detectedOS = OSType.MacOS;
+		} else if (OS.indexOf("win") >= 0) {
+			detectedOS = OSType.Windows;
+		} else if (OS.indexOf("nux") >= 0) {
+			detectedOS = OSType.Linux;
+		} else {
+			detectedOS = OSType.Other;
 		}
+
 		return detectedOS;
 	}
 }
