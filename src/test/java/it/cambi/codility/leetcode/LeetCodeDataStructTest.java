661078af0dbb5452298ac5363654227a4810f6cf
Luca Cambi <CambiL@c02y604vjhd5.traveljigsaw.local>
2020-02-06 16:01:13 +0000

    hash set design with node

--- src/test/java/it/cambi/codility/leetcode/LeetCodeDataStructTest.java
+++ src/test/java/it/cambi/codility/leetcode/LeetCodeDataStructTest.java
@@ -1,31 +1,95 @@
 /**
- * 
+ *
  */
 package it.cambi.codility.leetcode;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.jupiter.api.MethodOrderer.Alphanumeric;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * @author luca
  *
  */
 @TestMethodOrder(Alphanumeric.class)
-public class LeetCodeDataStructTest
-{
+public class LeetCodeDataStructTest {
 
-    class Employee
-    {
+    @Test
+    public void desigHashSet() {
+        MyHashSet hashSet = new MyHashSet();
+
+        hashSet.add(1);
+        hashSet.add(2);
+
+        assertTrue(hashSet.contains(1));
+        assertFalse(hashSet.contains(3));
+
+        hashSet.add(2);
+
+        assertTrue(hashSet.contains(2));
+
+        hashSet.remove(2);
+
+        assertFalse(hashSet.contains(2));
+
+
+    }
+
+
+    class Node {
+        int key;
+
+        Node(int key) {
+            this.key = key;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Node node = (Node) o;
+            return key == node.key;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(key);
+        }
+    }
+
+    class MyHashSet {
+
+        List<Node> list = new ArrayList<>();
+
+        /** Initialize your data structure here. */
+        public MyHashSet() {
+
+        }
+
+        public void add(int key) {
+            Node node = new Node(key);
+
+            if (!list.contains(node))
+                list.add(node);
+        }
+
+        public void remove(int key) {
+            list.remove(new Node(key));
+        }
+
+        /** Returns true if this set contains the specified element */
+        public boolean contains(int key) {
+            return list.contains(new Node(key));
+        }
+    }
+
+
+    class Employee {
         public int id;
         // the importance value of this employee
         public int importance;
@@ -37,36 +101,34 @@ public class LeetCodeDataStructTest
          * @param importance
          * @param subordinates
          */
-        public Employee(int id, int importance, List<Integer> subordinates)
-        {
+        public Employee(int id, int importance, List<Integer> subordinates) {
             super();
             this.id = id;
             this.importance = importance;
             this.subordinates = subordinates;
         }
 
-    };
+    }
+
+    ;
 
     AtomicInteger sum;
 
     @SuppressWarnings("serial")
     @Test
-    public void getImportance()
-    {
-        List<Employee> list = new ArrayList<Employee>()
-        {
+    public void getImportance() {
+        List<Employee> list = new ArrayList<Employee>() {
             {
-                add(new Employee(1, 5, new ArrayList<Integer>()
-                {
+                add(new Employee(1, 5, new ArrayList<Integer>() {
                     {
                         add(2);
                         add(3);
                     }
                 }));
 
-                add(new Employee(2, 3, Arrays.asList(new Integer[] {})));
+                add(new Employee(2, 3, Arrays.asList(new Integer[]{})));
 
-                add(new Employee(3, 3, Arrays.asList(new Integer[] {})));
+                add(new Employee(3, 3, Arrays.asList(new Integer[]{})));
             }
         };
 
@@ -82,8 +144,7 @@ public class LeetCodeDataStructTest
      * @param list
      * @return
      */
-    private AtomicInteger setImportance(List<Employee> list, int id)
-    {
+    private AtomicInteger setImportance(List<Employee> list, int id) {
         Employee employee = list.stream().filter(e -> e.id == id).findAny().get();
         sum = new AtomicInteger(employee.importance);
         sumImportance(list, employee.subordinates, sum);
@@ -91,10 +152,8 @@ public class LeetCodeDataStructTest
         return sum;
     }
 
-    public void sumImportance(List<Employee> employees, List<Integer> sub, AtomicInteger sum)
-    {
-        for (Integer integer : sub)
-        {
+    public void sumImportance(List<Employee> employees, List<Integer> sub, AtomicInteger sum) {
+        for (Integer integer : sub) {
             Employee employee = employees.stream().filter(e -> e.id == integer).findAny().get();
             sum.getAndAdd(employee.importance);
             sumImportance(employees, employee.subordinates, sum);
@@ -103,8 +162,7 @@ public class LeetCodeDataStructTest
 
     Map<Integer, Employee> emap = new HashMap<>();
 
-    public int getImportance(List<Employee> employees, int queryid)
-    {
+    public int getImportance(List<Employee> employees, int queryid) {
 
         for (Employee e : employees)
             emap.put(e.id, e);
@@ -112,8 +170,7 @@ public class LeetCodeDataStructTest
         return dfs(queryid);
     }
 
-    public int dfs(int eid)
-    {
+    public int dfs(int eid) {
         Employee employee = emap.get(eid);
         int ans = employee.importance;
         for (Integer subid : employee.subordinates)
