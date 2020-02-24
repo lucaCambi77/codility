08b9e2477d3c3d5e9c1bf9b41a58b22e3dd40613
Luca <lucacambi77@gmail.com>
2020-02-24 21:12:42 +0000

    check straight line

--- src/test/java/it/cambi/codility/leetcode/LeetCodeArrayTest.java
+++ src/test/java/it/cambi/codility/leetcode/LeetCodeArrayTest.java
@@ -37,11 +37,27 @@ public class LeetCodeArrayTest {
     @Test
     public void checkStraightLine() {
 
+        assertEquals(true, checkStraightLine(new int[][]{{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}}));
     }
 
-    public boolean checkStraightLine(int[][] coordinates) {
+    private boolean checkStraightLine(int[][] coordinates) {
+        int[] point1 = coordinates[1];
+        int[] point2 = coordinates[0];
+        float gSlope = slope(point1, point2);
 
-        return false;
+        for (int i = 1; i < coordinates.length; i++) {
+            int[] p1 = coordinates[i];
+            int[] p2 = coordinates[0];
+            float slop = slope(p1, p2);
+            if (gSlope != slop) return false;
+        }
+
+        return true;
+    }
+
+    private float slope(int[] p1, int[] p2) {
+        if ((p1[0] - p2[0]) == 0) return 0;
+        return (float) (p2[1] - p1[1]) / (p2[0] - p1[0]);
     }
 
     public void findNumbers() {
