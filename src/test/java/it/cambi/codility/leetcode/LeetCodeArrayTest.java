bcf7224d7f1a6ff45559514ab5abc5b5419dced1
lucacambi@rentalcars.com <lucacambi@rentalcars.com>
2020-04-03 14:26:22 +0200

    relative sort array

--- src/test/java/it/cambi/codility/leetcode/LeetCodeArrayTest.java
+++ src/test/java/it/cambi/codility/leetcode/LeetCodeArrayTest.java
@@ -35,6 +35,46 @@ public class LeetCodeArrayTest {
     private int[] nums;
 
     @Test
+    public void relativeSortArray() {
+        assertArrayEquals(new int[]{2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19}, relativeSortArray(new int[]{2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19}, new int[]{2, 1, 4, 3, 9, 6}));
+        assertArrayEquals(new int[]{22, 28, 8, 6, 17, 44}, relativeSortArray(new int[]{28, 6, 22, 8, 44, 17}, new int[]{22, 28, 8, 6}));
+        assertArrayEquals(new int[]{21, 11, 26, 20, 1, 18, 34, 50}, relativeSortArray(new int[]{26, 21, 11, 20, 50, 34, 1, 18}, new int[]{21, 11, 26, 20}));
+    }
+
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr2.length; i++) {
+            map.put(arr2[i], i);
+        }
+
+        arr1 = Arrays.stream(arr1).
+                boxed().
+                sorted(new Comparator<Integer>() {
+                    public int compare(Integer o1, Integer o2) {
+                        Integer left = map.get(o1);
+                        Integer right = map.get(o2);
+
+                        if (null != left && null != right)
+                            return left.compareTo(right);
+
+                        if (null != left && null == right)
+                            return -1;
+
+                        if (null == left && null != right)
+                            return 1;
+
+                        return o1.compareTo(o2);
+                    }
+                }).
+                mapToInt(i -> i).
+                toArray();
+
+        return arr1;
+    }
+
+    @Test
     public void countCharacters() {
 
         assertEquals(6, countCharacters(new String[]{"cat", "bt", "hat", "tree"}, "atach"));
