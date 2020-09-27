31a7441355d0ba7599d8ce12490d35dd940a8a54
Luca <lucacambi77@gmail.com>
2020-09-27 11:52:38 +0200

    adds sl4j lombok

--- src/test/java/it/cambi/codility/codility/Codility.java
+++ src/test/java/it/cambi/codility/codility/Codility.java
@@ -1,18 +1,17 @@
 package it.cambi.codility.codility;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.MethodOrderer.Alphanumeric;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(Alphanumeric.class)
+@Slf4j
 public class Codility {
-  private static final Logger log = LoggerFactory.getLogger(Codility.class);
 
   @Test
   public void perMissElement() {
