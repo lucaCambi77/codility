95282aa1edd9a275247090f5697416a36b1d1586
lucacambi@rentalcars.com <lucacambi@rentalcars.com>
2020-10-15 20:27:15 +0100

    updates power of

--- src/test/java/it/cambi/codility/coreJava/Recursion.java
+++ src/test/java/it/cambi/codility/coreJava/Recursion.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+/** */
 package it.cambi.codility.coreJava;
 
 import org.junit.jupiter.api.Test;
@@ -9,53 +7,41 @@ import java.math.BigInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * @author luca
- *
- */
-public class Recursion
-{
+/** @author luca */
+public class Recursion {
 
-    @Test
-    public void factorial()
-    {
+  @Test
+  public void factorial() {
 
-        assertEquals(120, factorial(new BigInteger("5")).intValue());
-        assertEquals("6227020800", factorial(new BigInteger("13")).toString());
-        assertEquals("265252859812191058636308480000000", factorial(new BigInteger("30")).toString());
-        assertEquals("3628800", factorial(new BigInteger("10")).toString());
+    assertEquals(120, factorial(new BigInteger("5")).intValue());
+    assertEquals("6227020800", factorial(new BigInteger("13")).toString());
+    assertEquals("265252859812191058636308480000000", factorial(new BigInteger("30")).toString());
+    assertEquals("3628800", factorial(new BigInteger("10")).toString());
+  }
 
-    }
+  public BigInteger factorial(BigInteger n) {
 
-    public BigInteger factorial(BigInteger n)
-    {
+    if (n.compareTo(new BigInteger("1")) == 0) return new BigInteger("1");
 
-        if (n.compareTo(new BigInteger("1")) == 0)
-            return new BigInteger("1");
+    String number = factorial(n.subtract(new BigInteger("1"))).toString();
 
-        String number = factorial(n.subtract(new BigInteger("1"))).toString();
+    return n.multiply(new BigInteger(number));
+  }
 
-        return n.multiply(new BigInteger(number));
-    }
+  @Test
+  public void power() {
+    assertEquals(32, power(2, 5));
+    assertEquals(1, power(2, 0));
+    assertEquals(64, power(2, 6));
+  }
 
-    @Test
-    public void power()
-    {
+  public long power(int a, int b) {
 
-        double n = 2;
-        int p = 5;
+    if (b == 0) return 1;
+    if (b == 1) return a;
 
-        assertEquals(32.0, power(n, p));
-
-    }
-
-    public double power(double n, int p)
-    {
-
-        if (p == 0)
-            return 1.0;
-
-        return n * power(n, p - 1);
-    }
-
+    long res = power(a, b / 2);
+    if (b % 2 == 0) return res * res;
+    return res * res * a;
+  }
 }
