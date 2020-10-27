3134492a3c6cfa318342f308b99fb05d075f01bc
lucacambi@rentalcars.com <lucacambi@rentalcars.com>
2020-10-27 21:36:55 +0000

    updates interpolation

--- core/src/main/java/it/cambi/codility/interpolation/Knn.java
+++ core/src/main/java/it/cambi/codility/interpolation/Knn.java
@@ -1,93 +1,81 @@
 /** */
 package it.cambi.codility.interpolation;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.IntStream;
 
 /** @author luca */
 public class Knn {
 
+  @AllArgsConstructor
+  @Getter
+  class Point {
+    int x;
+    int y;
+  }
+
+  // find a spiral around zero points
   public void impute(int[][] data) {
     int nosRows = data[0].length;
     int nosCols = data.length;
-    double items = (double) nosRows * nosCols;
-    /** Sample of neighbors */
-    double k = Math.sqrt(items);
 
-    int[] countRows = new int[nosRows];
-    Set<Integer> rowsWithZero = new HashSet<>();
-    Map<Integer, Integer> distanceMap = new HashMap<>();
+    Set<Point> rowsWithZero = new HashSet<>();
 
     for (int i = 0; i < data.length; i++) {
       int tmp = i;
-      long n =
-          IntStream.range(0, data[i].length)
-              .filter(v -> data[tmp][v] == 0)
-              .map(v -> ++countRows[v])
-              .count();
 
-      if (n > 0) rowsWithZero.add(i);
-
-      if (n == data[i].length) throw new RuntimeException("The whole row " + i + " is missing");
-
-      if (countRows[i] == data.length)
-        throw new RuntimeException("The whole column " + i + " is missing");
+      IntStream.range(0, data[i].length)
+          .filter(v -> data[tmp][v] == 0)
+          .mapToObj(v -> rowsWithZero.add(new Point(tmp, v)))
+          .count();
     }
 
-    for (Integer withZero : rowsWithZero) {
-      int[] x = data[withZero];
+    for (Point point : rowsWithZero) {
 
-      for (int j = 0; j < data.length; j++) {
-        int[] y = data[j];
-        for (int m = 0; m < x.length; m++) {
-          if (x[m] != 0 && y[m] != 0) {
-            int d = x[m] - y[m];
-            distanceMap.put(j, distanceMap.getOrDefault(j, 0) + d * d);
-          }
-        }
+      int x = point.getX();
+      int y = point.getY();
+      int sum = 0;
+
+      int k = 1;
+      int count = 0;
+      if (y + k < nosCols) {
+        sum += data[x][y + k];
+        count++;
+      }
+      if (y - k >= 0) {
+        sum += data[x][y - k];
+        count++;
+      }
+      if (x + k < nosRows) {
+        sum += data[x + k][y];
+        count++;
+      }
+      if (x - k >= 0) {
+        sum += data[x - k][y];
+        count++;
+      }
+      if (y - k >= 0 && x - k >= 0) {
+        sum += data[x - k][y - k];
+        count++;
+      }
+      if (x - k >= 0 && y + k < nosCols) {
+        sum += data[x - k][y + k];
+        count++;
+      }
+      if (y + k < nosCols && x + k < nosRows) {
+        sum += data[x + k][y + k];
+        count++;
+      }
+      if (x + k < nosRows && y - k >= 0) {
+        sum += data[x + k][y - k];
+        count++;
       }
 
-      Map<Integer, Integer> sortedMap =
-          distanceMap.entrySet().stream()
-              .sorted(Map.Entry.comparingByValue())
-              .collect(
-                  Collectors.toMap(
-                      Map.Entry::getKey,
-                      Map.Entry::getValue,
-                      (oldValue, newValue) -> oldValue,
-                      LinkedHashMap::new));
-
-      LinkedHashSet<Integer> neighbors = new LinkedHashSet<>();
-
-      double ktmp = k;
-      /** Create index of neighbors */
-      while (ktmp > 0) {
-
-        for (Map.Entry<Integer, Integer> aa : sortedMap.entrySet()) {
-          neighbors.add(aa.getKey());
-          ktmp--;
-        }
-      }
-
-      for (int j = 0; j < data[withZero].length; j++) {
-        if (x[j] == 0) {
-          int n = 0;
-          int m = 0;
-          int replace = 0;
-          while (m < nosCols) {
-            for (Integer neighbor : neighbors) {
-              if (data[neighbor][m] != 0) {
-                replace = replace + data[neighbor][m];
-                n++;
-              }
-            }
-            m++;
-          }
-
-          if (n > 0) x[j] = replace / n;
-        }
-      }
+      data[x][y] = sum / count;
     }
   }
 }
--- core/src/test/java/it/cambi/codility/interpolation/InterpolationTest.java
+++ core/src/test/java/it/cambi/codility/interpolation/InterpolationTest.java
@@ -9,7 +9,7 @@ public class InterpolationTest {
   private static Knn knn = new Knn();
 
   @Test
-  public void testKnn() {
+  public void should_find_knn() {
     int[][] data = new int[4][4];
     data[0][0] = 1;
     data[0][1] = 2;
@@ -33,6 +33,34 @@ public class InterpolationTest {
 
     knn.impute(data);
 
-    assertEquals(9, data[2][2]);
+    assertEquals(12, data[2][2]);
+  }
+
+  @Test
+  public void should_find_knn_1() {
+    int[][] data = new int[4][4];
+    data[0][0] = 0;
+    data[0][1] = 2;
+    data[0][2] = 3;
+    data[0][3] = 4;
+
+    data[1][0] = 6;
+    data[1][1] = 7;
+    data[1][2] = 8;
+    data[1][3] = 9;
+
+    data[2][0] = 10;
+    data[2][1] = 11;
+    data[2][2] = 0;
+    data[2][3] = 12;
+
+    data[3][0] = 15;
+    data[3][1] = 14;
+    data[3][2] = 13;
+    data[3][3] = 23;
+
+    knn.impute(data);
+
+    assertEquals(5, data[0][0]);
   }
 }
