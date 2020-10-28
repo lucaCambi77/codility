44203e973afa1ac01bd830c03efc9c316bda324b
lucacambi@rentalcars.com <lucacambi@rentalcars.com>
2020-10-28 13:34:04 +0000

    updates knn

--- core/src/main/java/it/cambi/codility/util/TestUtil.java
+++ core/src/main/java/it/cambi/codility/util/TestUtil.java
@@ -1,40 +1,11 @@
 package it.cambi.codility.util;
 
-public class AbstractTestUtil {
+public class TestUtil {
 
-  private String carriageReturn;
-
-  public enum OSType {
-    WINDOWS,
-    MAC_OS,
-    LINUX,
-    OTHER
-  }
-
-  public AbstractTestUtil() {
-    setCarriageReturn(System.getProperty("os.name", "generic").toLowerCase());
-  }
-
-  public void setCarriageReturn(String os) {
-
-    OSType osType = OSType.OTHER;
-
-    if ((os.contains("mac")) || (os.contains("darwin"))) {
-      osType = OSType.MAC_OS;
-    } else if (os.contains("win")) {
-      osType = OSType.WINDOWS;
-    } else if (os.contains("nux")) {
-      osType = OSType.LINUX;
-    }
-
-    String unixCarriage = "\n";
-    String windowsCarriage = "\r\n";
-
-    if (osType == OSType.WINDOWS) carriageReturn = windowsCarriage;
-    else carriageReturn = unixCarriage;
-  }
+  private String os = System.getProperty("os.name", "generic").toLowerCase();
 
   public String getCarriageReturn() {
-    return carriageReturn;
+    if (os.contains("win")) return "\r\n";
+    else return "\n";
   }
 }
--- core/src/test/java/it/cambi/codility/util/AbstractTestUtilTest.java
+++ core/src/test/java/it/cambi/codility/util/AbstractTestUtilTest.java
@@ -1,4 +1,33 @@
 package it.cambi.codility.util;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
 public class AbstractTestUtilTest {
+
+  @InjectMocks private TestUtil abstractTestUtil;
+
+  @BeforeAll
+  static void setUp() {
+    System.setProperty("os.name", "windows");
+  }
+
+  @Test
+  void should_match_carriage_return_by_system_property() {
+    ReflectionTestUtils.setField(abstractTestUtil, "os", "mac");
+    assertEquals("\n", abstractTestUtil.getCarriageReturn());
+  }
+
+  @Test
+  void should_match_carriage_by_os() {
+    ReflectionTestUtils.setField(abstractTestUtil, "os", "windows");
+    assertEquals("\r\n", abstractTestUtil.getCarriageReturn());
+  }
 }
