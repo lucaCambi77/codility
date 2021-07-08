fdf445681f81940014d31fce9fd6ea989a8740c9
LUCA <lucacambi77@gmail.com>
2021-07-08 12:46:17 +0200

    adds twoSumIII

--- leetcode/src/test/java/it/cambi/codility/leetcode/LeetCodeDataStructTest.java
+++ leetcode/src/test/java/it/cambi/codility/leetcode/LeetCodeDataStructTest.java
@@ -14,6 +14,48 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.MethodName.class)
 class LeetCodeDataStructTest {
 
+  static class TwoSum {
+    private final HashMap<Integer, Integer> num_counts;
+
+    /** Initialize your data structure here. */
+    public TwoSum() {
+      this.num_counts = new HashMap<Integer, Integer>();
+    }
+
+    /** Add the number to an internal data structure.. */
+    public void add(int number) {
+      if (this.num_counts.containsKey(number))
+        this.num_counts.replace(number, this.num_counts.get(number) + 1);
+      else this.num_counts.put(number, 1);
+    }
+
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    public boolean find(int value) {
+      for (Map.Entry<Integer, Integer> entry : this.num_counts.entrySet()) {
+        int complement = value - entry.getKey();
+        if (complement != entry.getKey()) {
+          return this.num_counts.containsKey(complement);
+        } else {
+          if (entry.getValue() > 1) return true;
+        }
+      }
+
+      return false;
+    }
+  }
+
+  @Test
+  public void twoSumIII() {
+
+    TwoSum twoSum = new TwoSum();
+    twoSum.add(1);
+    twoSum.add(3);
+    twoSum.add(5);
+
+    assertTrue(twoSum.find(4));
+    assertFalse(twoSum.find(7));
+  }
+
   public class ZigzagIterator {
 
     Queue<Iterator<Integer>> q;
